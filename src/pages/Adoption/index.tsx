c4590d5d66f7f459f76f80492a0e1967ff6c9dbf
natcurti <naticurti01@gmail.com>
2024-05-19 11:19:14 -0300

    Fetch animals with thunk middleware

--- src/pages/Adoption/index.tsx
+++ src/pages/Adoption/index.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import ContainerInnerPage from "src/components/ContainerInnerPage";
 import Text from "src/components/Text";
+import { getAnimals } from "src/store/reducers/sliceAnimals";
+import { useAppDispatch, useAppSelector } from "src/types/hooks";
 
 const Adoption = () => {
+  const animals = useAppSelector((state) => state.animals);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAnimals());
+  }, [dispatch]);
+
+  console.log(animals);
+
   return (
     <ContainerInnerPage isLoggedIn>
       <Text color="blue">Olá! Veja os amigos disponíveis para adoção!</Text>
--- src/store/reducers/sliceAnimals.ts
+++ src/store/reducers/sliceAnimals.ts
@@ -1,12 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import animalsAvaliable from "src/services/animalsAvaliable";
 import { IAnimals } from "src/types/IAnimals";
 
 const initialState: IAnimals[] = [];
+
+export const getAnimals = createAsyncThunk(
+  "animals/get",
+  animalsAvaliable.getAnimals
+);
 
 const sliceAnimals = createSlice({
   name: "animals",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAnimals.fulfilled, (state, { payload }) => {
+      state.push(payload);
+    });
+  },
 });
 
 export default sliceAnimals.reducer;
